b597cb28915fe31d0b04362538bc70daf1d1d7d4
Armando de Oliveira Gonçalves Neto <106642163+ArmandoN3to@users.noreply.github.com>
2024-10-15 17:58:04 -0400

    Update formContato.py

---
+++ modules/formContato.py
@@ -0,0 +1,11 @@
+from formsbase import FormsBase
+
+class FormContato(FormsBase):
+    def __init__(self, cpf, nome, data_nascimento,telefone,celular, email:str):
+        self.telefone = telefone
+        self.celular = celular
+        self.email =email
+        super().__init__(cpf, nome, data_nascimento)
+    
+    def preencher_forms(self):
+        return print(f'nome: {self.nome} cpf: {self.cpf} data de nascimento: {self.data_nascimento} Telefone: {self.telefone} Celular: {self.celular} Email: {self.email}')
