a8a5451f489bada3a9cf00926fd8eafc4d10fc34
Armando de Oliveira Gonçalves Neto <106642163+ArmandoN3to@users.noreply.github.com>
2024-10-15 17:08:52 -0400

    Create formsbase.py

---
+++ modules/formsbase.py
@@ -0,0 +1,32 @@
+class FormsBase:
+    def __init__(self,cpf,nome,data_nascimento):
+        self.__cpf =cpf
+        self.__nome = nome
+        self.__data_nascimento = data_nascimento
+    
+    @property
+    def cpf(self):
+        return self.__cpf
+    
+    @cpf.setter
+    def cpf(self,novo_cpf):
+        self.__cpf = novo_cpf
+        
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self,novo_nome):
+        self.__nome = novo_nome 
+
+    @property
+    def data_nascimento(self):
+        return self.__data_nascimento
+    
+    @data_nascimento.setter
+    def data_nascimento(self,novo_data_nascimento):
+        self.__data_nascimento = novo_data_nascimento 
+
+
+    
