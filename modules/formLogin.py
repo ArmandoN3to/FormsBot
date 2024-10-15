2daf748e530cf3d6b4aa94892a07c5226f21be77
BrenerCantuaria <brenercantuariasantos@gmail.com>
2024-10-15 17:14:26 -0400

    Create formLogin.py

---
+++ modules/formLogin.py
@@ -0,0 +1,8 @@
+from formsbase import FormsBase
+
+class Login(FormsBase):
+    def __init__(self, cpf, nome, data_nascimento,login,senha):
+        self.login = login
+        self.__senha = senha
+        super().__init__(cpf, nome, data_nascimento)
+    
