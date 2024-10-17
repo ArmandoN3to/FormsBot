00f72cd76b63125d1972892e8eb036b77329c847
Armando de Oliveira Gonçalves Neto <106642163+ArmandoN3to@users.noreply.github.com>
2024-10-17 12:05:16 -0400

    Update debug.py

--- debug.py
+++ debug.py
@@ -4,7 +4,9 @@ from modules.formLogin import FormsLogin
 
 login = FormsLogin('060.445.262-40','Armando','02/02/2003','armando','1234')
 login.preencher_forms()
+
 endereco = FormEndereco('060.445.262-40','Armando','02/02/2003','69070120','Rio Branco','65','Educado','manacapuru')
 endereco.preencher_forms()
+
 contato = FormContato('060.445.262-40','Armando','02/02/2003','3232-1033',celular='99923-2932',email='emaildoarmando@hotmail.com')
 contato.preencher_forms()
